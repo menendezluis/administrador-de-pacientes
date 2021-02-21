28ed7f024b1d28fe037c6515b1d1721e71e78442
Luis Menendez <lmenendez.1@gmail.com>
2021-02-20 22:56:33 -0600

    Update ListadoCitas.js

--- src/components/ListadoCitas.js
+++ src/components/ListadoCitas.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function ListadoCitas(){
+export default function ListadoCitas({registro}){
     return(
      <div className="cita">
-         <p>Mascota: <span></span></p>
+         <p>Mascota: <span>{registro.name}</span></p>
+         <p>Dueño: <span>{registro.lastname}</span></p>
+         <p>Email: <span>{registro.email}</span></p>
+         <p>Phone: <span>{registro.phone}</span></p>
+         <p>Fecha: <span>{registro.date}</span></p>
+         <p>Hora: <span>{registro.time}</span></p>
+         <p>Sintomas: <span>{registro.sintomas}</span></p>
      </div>
     )
 }
